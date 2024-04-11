30277b998fc394c845c8230bd0dd524a4373c310
Alaa Yahia <alaay873@live.com>
2024-04-11 13:10:30 +0200

    day3-task1.1

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,16 @@
+import { Tag } from '@dhis2/ui'
 import React from 'react'
+import { useConfig } from '@dhis2/app-runtime'
 
-export const Home = () => (
-    <div>
-        <h1>Home</h1>
+export const Home = () => {
+    const { apiVersion } = useConfig()
+    return (
+        <div>
+            <h1>Home</h1>
 
-        <p>DHIS2 Web App Academy 2024</p>
-    </div>
-)
+            <p>DHIS2 Web App Academy 2024</p>
+
+            <Tag positive>Api Version: {apiVersion}</Tag>
+        </div>
+    )
+}
